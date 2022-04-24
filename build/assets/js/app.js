632fb305f2fdd6e3a60d212813dd8176df86e962
AlexKrivets <krivets2005@gmail.com>
2022-04-24 13:04:59 +0300

    added mixin

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,3 +1,20 @@
+console.log(document);
+
+const nav = document.getElementById('nav');
+const navClose = document.getElementById('nav-close');
+const navOpen = document.getElementById('nav-open');
+
+console.log(nav);
+
+window.ontouchstart = function(event) {
+    if ((event.target = navOpen)) {
+        nav.classList.add('active');
+    }
+    if ((event.target = navClose)) {
+        nav.classList.remove('active');
+    }
+};
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     // direction: 'vertical',
